96c4a8bcfbdd56063751064a9db4546392e30a28
[SanjayJDM] <[sanjayjdm@gmail.com]>
2020-12-06 16:52:40 +0000

    stayall

--- src/christmaslights.py
+++ src/christmaslights.py
@@ -386,5 +386,5 @@ def api_status():
     return response
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=3000, debug=True)
     stayAll(0.3)
+    app.run(host='0.0.0.0', port=3000, debug=True)
