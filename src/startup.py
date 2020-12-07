ab13d7d9110f6f4d1308cfc4b167a815b7dc0990
[SanjayJDM] <[sanjayjdm@gmail.com]>
2020-12-07 14:58:35 +0000

    rerurn 0.0.0.0

--- src/startup.py
+++ src/startup.py
@@ -5,6 +5,6 @@ import json
 import time
 import urllib2
 
-url='http://121.0.0.1:3000/christmaslight/stayall/'
+url='http://0.0.0.0:3000/christmaslight/stayall/'
 jsonData = urllib2.urlopen(url).read()
 print  jsonData
