687669291b15aa06a31f71e32a937295010916d0
[SanjayJDM] <[sanjayjdm@gmail.com]>
2020-12-07 11:52:58 +0000

    added local host

--- src/startup.py
+++ src/startup.py
@@ -5,6 +5,6 @@ import json
 import time
 import urllib2
 
-url='http://192.168.0.39:3000/christmaslight/stayall/'
+url='http://121.0.0.1:3000/christmaslight/stayall/'
 jsonData = urllib2.urlopen(url).read()
 print  jsonData
